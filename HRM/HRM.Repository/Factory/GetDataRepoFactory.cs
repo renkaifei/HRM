8f33a9f1a737a75f163a6e238618880cb42311d0
renkaifei <18910891894@163.com>
2020-11-10 16:12:02 +0800

    创建获取数据工厂

---
+++ HRM/HRM.Repository/Factory/GetDataRepoFactory.cs
@@ -0,0 +1,38 @@
+using System;
+using HRM.Domain;
+using HRM.Repository.Interface;
+
+namespace HRM.Repository.Factory
+{
+    public class GetDataRepoFactory
+    {
+        private static GetDataRepoFactory instance;
+        private static object obj = new object;
+        
+        private GetDataRepoFactory()
+        {
+
+        }
+
+        public static GetDataRepoFactory GetInstance()
+        {
+            if(instance == null)
+            {
+                lock(obj)
+                {
+                    if(instance == null)
+                    {
+                        instance = new GetDataRepoFactory();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public IGetDataRepository BuildUserGetOneByNameWithPwdRepository(UserDomain user)
+        {
+            IGetDataRepository getData = new UserGetOneByNameWithPwdRepository(user);
+            return getData;
+        }
+    }
+}
